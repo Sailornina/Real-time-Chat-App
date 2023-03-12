cb04895434368363e1df9f3678b7262d0d7d1afa
Sailornina <sylcardozo.sc@gmail.com>
2023-03-12 18:39:56 +0100

    Home page is done. useNavigate was added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import './index.css';
+import { useState } from 'react';
+import { io } from 'socket.io-client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
+const socket = io("ws://localhost:3000");
+
 export const App = () => {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={
+            <Home
+              username={username}
+              setUsername={setUsername}
+              room={room}
+              setRoom={setRoom}
+              socket={socket}
+            />}>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,25 @@
 import './home.css';
+import { useNavigate } from 'react-router-dom'; 
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+
+    const joinRoom = () => {
+        if (room !== '' && username !== '') {
+          socket.emit('join_room', { username, room });
+        }
+    };
+
+    const navigate = useNavigate();
+
+    navigate('/chat', { replace: true });
+
     return (
         <div className="container">
             <div className="formContainer">
-                <h1>{`WineLovers`}</h1>
-                <input className="input" placeholder='Username...' />
+                <h1>WineLovers</h1>
+                <input className="input" placeholder='Username...' onChange={(e) => setUsername(e.target.value)}/>
 
-                <select className="input option">
+                <select className="input option" onChange={(e) => setRoom(e.target.value)}>
                     <option>-- Select Room --</option>
                     <option value='malbec'>Malbec</option>
                     <option value='pinot-noir'>Pinot Noir</option>
@@ -15,7 +27,7 @@ const Home = () => {
                     <option value='cinsaut'>Cinsaut</option>
                 </select>
 
-                <button className="btn btn-secondary">Join Room</button>
+                <button className="btn btn-secondary" onClick={joinRoom}>Join Room</button>
             </div>
         </div>
     );
