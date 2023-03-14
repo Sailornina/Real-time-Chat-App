37c5e3b829805679bbf25af28c8d473b7818148d
Sailornina <sylcardozo.sc@gmail.com>
2023-03-14 14:22:10 +0100

    First Socket.io Event Listener was created

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 import express from "express";
 import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const port = process.env.PORT || 8080;
 const app = express();
+const httpServer = createServer();
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hi');
+    res.send('Hi everyone!');
+});
+
+const io = new Server(httpServer, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
 });
 
 app.listen(port, () => {
