0da56349663c58f1a379bd2754b987a12e0a240a
Sailornina <sylcardozo.sc@gmail.com>
2023-03-17 16:30:46 +0100

    Messages.js in progress. Moment.js was added

--- frontend/src/components/rooms/Messages.js
+++ frontend/src/components/rooms/Messages.js
@@ -1,9 +1,34 @@
 import './chat.css';
+import moment from 'moment';
+import { useState, useEffect } from 'react';
 
-const Messages = () => {
+const Messages = ({ socket }) => {
+    const [messagesRecieved, setMessagesReceived] = useState([]);
+
+    useEffect(() => { // a socket event is recieved from the server.
+        socket.on('messageResponse', (data) =>
+            setMessagesReceived([
+                ...messagesRecieved,
+                data.message,
+                data.username,
+                data.createdtime
+            ]));
+    }, [socket, messagesRecieved]);
+
     return (
-      <div className='messagesColumn'>
-        Hi Messages
+        <div className="messagesColumn">
+        {messagesRecieved.map((msg, item) => (
+          <div className="message" key={item}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <span className="msgUser">{msg.username}</span>
+              <span className="msgUser">
+                {moment(msg.createdtime).format('h:mma')}
+              </span>
+            </div>
+            <p className="msgText">{msg.message}</p>
+            <br />
+          </div>
+        ))}
       </div>
     );
 };
