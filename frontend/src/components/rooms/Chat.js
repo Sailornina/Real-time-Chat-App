4dc37b30a535664b7ed3d314da851d622b4094b3
Sailornina <sylcardozo.sc@gmail.com>
2023-03-17 16:42:06 +0100

    Chat.js in progress

--- frontend/src/components/rooms/Chat.js
+++ frontend/src/components/rooms/Chat.js
@@ -1,11 +1,14 @@
 import './chat.css';
+import Messages from './Messages';
 
-const Chat = () => {
-    return (
-      <div className='chat-container'>
-        Hi Chat!!
+const Chat = ({ socket }) => {
+  return (
+    <div className="chatContainer">
+      <div>
+        <Messages socket={socket} />
       </div>
-    );
+    </div>
+  );
 };
 
 export default Chat;
