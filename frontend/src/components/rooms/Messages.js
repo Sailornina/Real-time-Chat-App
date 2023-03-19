3c209191a3b12a38182b6e59eda89d72f3151d8d
Sailornina <sylcardozo.sc@gmail.com>
2023-03-19 14:30:57 +0100

    events socket.io was fixed

--- frontend/src/components/rooms/Messages.js
+++ frontend/src/components/rooms/Messages.js
@@ -3,34 +3,38 @@ import moment from 'moment';
 import { useState, useEffect } from 'react';
 
 const Messages = ({ socket }) => {
-    const [messagesRecieved, setMessagesReceived] = useState([]);
+  const [messagesRecieved, setMessagesReceived] = useState([]);
 
-    useEffect(() => { // a socket event is recieved from the server.
-        socket.on('receive_message', (data) =>
-            setMessagesReceived([
-                ...messagesRecieved,
-                data.message,
-                data.username,
-                data.createdtime
-            ]));
-    }, [socket, messagesRecieved]);
+  useEffect(() => { // a socket event is recieved from the server.
+    socket.on('receive_message', (data) => {
+      setMessagesReceived((prevMessagesReceived) => [
+        ...prevMessagesReceived,
+        {
+          message: data.message,
+          username: data.username,
+          createdTime: data.createdtime
+        }
+      ])
+    });
+    return () => socket.off('receive_message');
+  }, [socket]);
 
-    return (
-        <div className="messagesColumn">
-        {messagesRecieved.map((msg, item) => (
-          <div className="message" key={item}>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <span className="msgUser">{msg.username}</span>
-              <span className="msgUser">
-                {moment(msg.createdtime).format('h:mma')}
-              </span>
-            </div>
-            <p className="msgText">{msg.message}</p>
-            <br />
+  return (
+    <div className="messagesColumn">
+      {messagesRecieved.map((msg, item) => (
+        <div className="message" key={item}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <span className="msgUser">{msg.username}</span>
+            <span className="msgUser">
+              {moment(msg.createdtime).format('h:mma')}
+            </span>
           </div>
-        ))}
-      </div>
-    );
+          <p className="msgText">{msg.message}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Messages;
